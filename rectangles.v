d21845a18ec2cc3ba2ba1e4f34129bcd125278ff
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-10 05:12:19 +0900

    first commit

---
+++ rectangles.v
@@ -0,0 +1,38 @@
+module main
+
+import gg
+
+const win_width = 600
+const win_height = 300
+
+struct App {
+mut:
+	gg    &gg.Context = unsafe { nil }
+}
+
+fn main() {
+	mut app := &App{}
+	app.gg = gg.new_context(
+		bg_color:      gg.white
+		width:         win_width
+		height:        win_height
+		create_window: true
+		window_title:  'Rectangles'
+		frame_fn:      frame
+		user_data:     app
+	)
+	app.gg.run()
+}
+
+fn frame(app &App) {
+	app.gg.begin()
+	app.draw()
+	app.gg.end()
+}
+
+fn (app &App) draw() {
+	// app.gg.draw_text_def(200,20, 'hello world!')
+	// app.gg.draw_text_def(300,300, 'привет')
+	app.gg.draw_rect_filled(10, 10, 100, 30, gg.blue)
+	app.gg.draw_rect_empty(110, 150, 80, 40, gg.black)
+}
